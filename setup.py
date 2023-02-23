cb992b0061826647d3910181a0de7285a4e01242
Rene Lazaro Collado Arteaga <rene_collado@inca.edu.cu>
2023-02-23 14:58:32 -0500

    cambios reales
    2-23-2023

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+
+	name="ReneTkinter",
+	version="1.0",
+	description="visual de Python",
+	author="Rene",
+	author_email="renearteaga261998@gmail.com",
+	packages=["Tkinters","Tkinters.ClasesUtiles","Tkinters.ClasesUtiles.Tipos","Tkinters.ClasesUtiles.Componentes","Tkinters.ClasesUtiles.Interfaces","Tkinters.Imports"],
+	url="www.google.com"
+	
+	)
